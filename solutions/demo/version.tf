55f8ffc2e00db815eced463ac37515805ac24cfb
Alex Reiff <alex.reiff@ibm.com>
2025-06-26 10:38:18 -0400

    m

--- solutions/demo/version.tf
+++ solutions/demo/version.tf
@@ -6,7 +6,7 @@ terraform {
   required_providers {
     ibm = {
       source  = "ibm-cloud/ibm"
-      version = "1.77.0" # encountering a plugin panic at 1.79.2
+      version = "1.79.2"
     }
     kubectl = {
       source  = "gavinbunney/kubectl"
